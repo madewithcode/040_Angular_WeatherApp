721fe4f18f38826e97d1a4fb50de3fdfc52a0b0e
madewithcode <ahuja.software@gmail.com>
2015-12-20 10:38:42 -0500

    VA 13 - weather app - part 4

--- app.js
+++ app.js
@@ -12,30 +12,64 @@ weatherApp.config(function ($routeProvider) {
             templateUrl: 'pages/forecast.htm',
             controller: 'forecastController'
         })
+        .when('/forecast/:days', {
+            templateUrl: 'pages/forecast.htm',
+            controller: 'forecastController'
+        })
 });
 
 // Services
-weatherApp.service('cityService', function() {
+weatherApp.service('cityService', function () {
     this.city = 'New York, NY';
 });
 
 // Controllers
 weatherApp.controller('homeController', ['$scope', 'cityService',
-    function($scope, cityService) {
+    function ($scope, cityService) {
 
         $scope.city = cityService.city;
 
         // watch the value for 'city' as it may change on the home page (text box)
-        $scope.$watch('city', function() {
-           cityService.city = $scope.city;
+        $scope.$watch('city', function () {
+            cityService.city = $scope.city;
         });
 
 
-}]);
+    }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService',
-    function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService',
+    function ($scope, $resource, $routeParams, cityService) {
 
         $scope.city = cityService.city;
+        $scope.days = $routeParams.days || 2; // default to 2
 
-}]);
+        // make api call. doing it this way will also prevent the CORS errors.
+        // example: http://api.openweathermap.org/data/2.5/forecast/daily?q=London&cnt=2&APPID=2a76523ae46789c184a204e0d5ec5101
+        $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=2a76523ae46789c184a204e0d5ec5101',
+            {
+                callback: 'JSON_CALLBACK'
+            },
+            {
+                get: {
+                    method: 'JSONP'
+                }
+            }
+        );
+
+        $scope.weatherResult = $scope.weatherAPI.get(
+            {
+                q: $scope.city,
+                cnt: $scope.days
+            }
+        );
+
+        $scope.convertToF = function(degK) {
+            return Math.round(1.8 * (degK - 273) + 32);
+        };
+
+        $scope.convertToDate = function(dt) {
+            return new Date(dt * 1000);
+        };
+
+
+    }]);
