951e7f57dbee0468a7c6faaf5d5ed81548ba60b0
madewithcode <ahuja.software@gmail.com>
2016-01-15 11:28:44 -0500

    VA 19 - moved the forecast api call from controller to services

--- forecastController.js
+++ forecastController.js
@@ -1,32 +1,12 @@
 // forecastController
 
-
-
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService',
-    function ($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService',
+    function ($scope, $routeParams, cityService, weatherService) {
 
         $scope.city = cityService.city;
         $scope.days = $routeParams.days || '2'; // default to 2
 
-        // make api call. doing it this way will also prevent the CORS errors.
-        // example: http://api.openweathermap.org/data/2.5/forecast/daily?q=London&cnt=2&APPID=2a76523ae46789c184a204e0d5ec5101
-        $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=2a76523ae46789c184a204e0d5ec5101',
-            {
-                callback: 'JSON_CALLBACK'
-            },
-            {
-                get: {
-                    method: 'JSONP'
-                }
-            }
-        );
-
-        $scope.weatherResult = $scope.weatherAPI.get(
-            {
-                q: $scope.city,
-                cnt: $scope.days
-            }
-        );
+        $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
 
         $scope.convertToF = function (degK) {
             return Math.round(1.8 * (degK - 273) + 32);
--- services.js
+++ services.js
@@ -2,3 +2,31 @@
 weatherApp.service('cityService', function () {
     this.city = 'New York, NY';
 });
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+
+    this.GetWeather = function(city, days) {
+
+        // make api call. doing it this way will also prevent the CORS errors.
+        // example: http://api.openweathermap.org/data/2.5/forecast/daily?q=London&cnt=2&APPID=2a76523ae46789c184a204e0d5ec5101
+        var weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=2a76523ae46789c184a204e0d5ec5101',
+            {
+                callback: 'JSON_CALLBACK'
+            },
+            {
+                get: {
+                    method: 'JSONP'
+                }
+            }
+        );
+
+        return weatherAPI.get(
+            {
+                q: city,
+                cnt: days
+            }
+        );
+
+    };
+
+}]);
